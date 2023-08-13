9ebff2e98a363a469f4e0e9d0e1e0bc9ae6c06ec
Muxammedali <77286466+Muxammedali@users.noreply.github.com>
2023-08-13 19:44:46 +0400

    Add files via upload

---
+++ Css/for study/js/main.js
@@ -0,0 +1,24 @@
+/*Рекурсия*/
+
+                  function fact (n)
+                  {
+                     if (n== 1){
+                        return 1;
+                     } 
+                     else {
+                        return n * fact(n-1);
+                     }
+
+
+                  }
+
+                  alert(fact(5));
+
+
+
+
+
+
+
+
+ 
